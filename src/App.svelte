bac23216fe305da0fdc0a78c881ac25d4d092e18
Luke Harby <Luke.Harby@akqa.com>
2023-06-19 14:22:13 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'Luke';
+	let age = 20;
+
+	function incrementAge() {
+		age += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: #555;
+	}
+</style>
+
+<h1>Hello {name}! My age is {age}!</h1>
+<button on:click="{incrementAge}">Change age</button>
