11509fee42433372a9ca323e79715b871ff24a5b
Luke Harby <Luke.Harby@akqa.com>
2023-06-21 08:52:20 +0100

    Push new changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,38 @@
 <script>
 	let name = 'Luke';
-	let age = 20;
+	let age = 50;
 
 	function incrementAge() {
 		age += 1;
 	}
+
+	function changeName() {
+		name = 'Susan';
+	}
+
+	function nameInput(event) {
+		const enteredValue = event.target.value;
+		name = enteredValue;
+	}
+
+	$: uppercaseName = name.toUpperCase();
+
+	$: console.log(name);
+
+	$: if (name === 'Susan') {
+		age = 55;
+	}
+
 </script>
 
 <style>
 	h1 {
-		color: #555;
+		color: #333;
 	}
 </style>
 
-<h1>Hello {name}! My age is {age}!</h1>
-<button on:click="{incrementAge}">Change age</button>
+<h1>Hello {uppercaseName}! My age is {age}!</h1>
+<button on:click={incrementAge}>Increment age</button>
+<!-- <button on:click={changeName}>Change name</button> -->
+<!-- <input type="text" value={name} on:input={nameInput}> -->
+<input type="text" bind:value={name}>
